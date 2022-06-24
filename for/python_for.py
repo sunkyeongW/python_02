2afc64736985ec20840d84c9f2974981884af414
sunkyeongW <bluelight0949@naver.com>
2022-06-24 17:53:38 +0900

    Add document content

--- for/python_for.py
+++ for/python_for.py
@@ -35,3 +35,94 @@ names = ['Kim', 'Park', 'Won']
 
 for n in names:
     print('You are:', n)
+print()
+
+#예제4
+lotto = [14, 45, 56, 26, 56, 78]
+
+for l in lotto:
+    print("Currnet number :", l)
+
+#예제5
+word = 'Beautiful'
+
+for s in word:
+    print('word :', s)
+print()
+
+#예제6
+my_info = {
+    "Name": 'Won',
+    "Age": 26,
+    "City": 'Seoul'
+}
+print()
+for w in my_info:
+    print('key :', my_info.get(w))
+print()
+for key in my_info:
+    print('key :', my_info[key])
+print()
+for v in my_info.values():
+    print('values :', v)
+print()
+
+#예제6
+name = 'FineAppLE'
+
+for n in name:
+    if n.isupper():
+        print(n)
+    else:
+        print(n.upper())
+print()
+
+#예제7
+#break
+
+numbers = [14, 4, 6, 78, 34, 67, 12]
+
+for n in numbers:
+    if n == 6:
+        print('Found : 34!')
+        break
+    else:
+        print('No :', n)
+
+print()
+
+#continue
+
+lt = ["1", 2, 5, True, 3.3, complex(4)]
+
+for v in lt:
+    if type(v) is bool:
+        continue
+    print("Current type:", v, type(v))
+print()
+
+#예제8
+#for- else
+
+numbers = [14, 4, 6, 78, 34, 67, 12]
+
+for num in numbers:
+    if num == 99:
+        print("Found!")
+        break
+else:
+    print("Not Found")
+
+#예제9
+for i in range(2,10):
+    for j in range(1,10):
+        print('{:4d}'.format(i *j), end='')
+    print()
+
+#변환 예제
+name2 = 'Acyue'
+
+print('Reversed', reversed(name2))
+print('List', list(reversed(name2)))
+print('Tuple', tuple(reversed(name2)))
+print('Set', set(reversed(name2)))
--- for/readme_for.md
+++ for/readme_for.md
@@ -8,3 +8,24 @@ For in <collection>
 #문자열, 리스트, 튜플, 집합, 사전(딕셔너리)
 #iterable 리턴 함수 : range, reversed, enumerate, filter, map, zip
 
+#함수
+isupper: 대문자인지 확인하는 함수
+islower: 소문자인지 확인하는 함수
+upper: 대문자로 변환
+
+#break
+제어흐름을 중단
+if 문에서 많이 활용
+
+#continue
+반복문 안에 명시된 수행 코드를 건너뜀
+is - 변수가 같은 객체를 가르킴
+== - 변수가 같은 값을 가짐
+
+#for-else
+반복문을 모두 마친 후에 else 문을 실행
+-> for문에서 break가 발생하지 않았을 경우”의 쓰임
+
+#reversed
+뒤집어서 출력
+set은 순서대로 출력x
