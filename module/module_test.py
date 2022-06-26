5653680c3c277f5ef50573924b12341804ef1186
sunkyeongW <bluelight0949@naver.com>
2022-06-26 20:30:11 +0900

    Prevents code errors from receiving additional arguments during development.

---
+++ module/module_test.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/mnt/c/Users/sunkyeong/workspace/python_02/math')
+
+print(sys.path)
+
+
+
+
---
+++ module/python_module.py
@@ -0,0 +1,25 @@
+#예제1
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+print('-' * 15)
+print('called! inner!')
+print(add(5,5))
+print(subtract(5,5))
+print(multiply(5,5))
+print(divide(5,5))
+print(power(5,5))
+print('-' * 15)
