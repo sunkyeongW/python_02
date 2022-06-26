df86588106a194e1a410e5b840dd7107b122593c
sunkyeongW <bluelight0949@naver.com>
2022-06-26 13:29:30 +0900

    add input directory, create python_input.py and readme_input.md

---
+++ input/python_input.py
@@ -0,0 +1,22 @@
+#예제1
+#Name = input("Enter your Name :")
+#Age = input("Enter your Age :")
+
+#print(Name, Age)
+#print()
+
+#예제2(int형)
+#first_number = int(input("Enter number1 :"))
+#second_number = int(input("Enter number2 :"))
+
+#total = first_number + second_number
+#print("first_number + second_number :", total)
+
+#예제3(float형)
+#float_number = float(input("Enter float_number :"))
+
+#print("input float :", float_number)
+#print("input type :", type(float_number))
+
+#예제4
+print("FirstName - {0} - LastName - {1}".format(input("Enter first name :"), input("Enter second name :")))
